1bdfb41706a19824dfdb816bc163f981f434762f
Jonathan Luk <jluk87@gmail.com>
2018-04-05 22:32:11 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,10 +41,8 @@ class Song
 
   def new_from_filename(filename)
       song_data = filename.gsub(".mp3","").split(" - ")
-      artist = song_data[0]
       song = song_data[1]
-      genre = song_data[2]
-      Song.new(song, artist, genre)
+      find_or_create_by_name(song)
   end
 
   def create_from_filename(filename)
