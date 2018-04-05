278bdef70c8ebf3e2d6027e8de933d7a0e54ea09
Jonathan Luk <jluk87@gmail.com>
2018-04-05 22:16:46 +0000

    automatically backed up by learn

---
+++ lib/musicimporter.rb
@@ -0,0 +1,28 @@
+require 'pry'
+class MusicImporter
+
+  attr_accessor :files
+  attr_reader :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    @files ||= Dir.glob('#{self.path}/*.mp3').collect {|song| song.gsub('#{self.path}','')}
+  end
+
+  def import
+    files.each do |filename|
+      song_data = filename.gsub('.mp3','').split(' - ')
+      artist = song_data[0]
+      song = song_data[1]
+      genre = song_data[2]
+      Song.new(song, artist, genre)
+    end
+  end
+
+end
+
+new_import = MusicImporter.new('./db/mp3s')
+binding.pry
---
+++ lib/song.rb
@@ -0,0 +1,54 @@
+class Song
+
+  extend Concerns::Findable
+
+  attr_accessor :name
+  attr_reader :artist, :genre
+
+  @@all = []
+
+  def initialize(name, artist=nil, genre=nil)
+    @name = name
+      self.artist = artist if artist != nil
+      self.genre = genre if genre != nil
+  end
+
+  def artist=(artist)
+      @artist = artist
+      artist.add_song(self) unless artist.songs.include?(self)
+  end
+
+  def genre=(genre)
+      @genre = genre
+      genre.add_song(self) unless genre.songs.include?(self)
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+  def self.create(name)
+    Song.new(name).tap {|new_instance| new_instance.save}
+  end
+
+  def new_from_filename(filename)
+      song_data = filename.gsub('.mp3','').split(' - ')
+      artist = song_data[0]
+      song = song_data[1]
+      genre = song_data[2]
+      Song.new(song, artist, genre)
+  end
+
+  def create_from_filename(filename)
+    new_from_filename(filename).tap {|file| file.save}
+  end
+
+end
