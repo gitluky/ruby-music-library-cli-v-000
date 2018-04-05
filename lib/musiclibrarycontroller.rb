b1a195087c00db13a3c4fe48fa8c39404d148dca
Jonathan Luk <jluk87@gmail.com>
2018-04-05 23:22:22 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -9,39 +9,39 @@ class MusicLibraryController
     input = nil
     while input != 'exit'
 
-    puts "Welcome to your music library!"
-    puts "To list all of your songs, enter 'list songs'."
-    puts "To list all of your songs, enter 'list songs'."
-    puts "To list all of the artists in your library, enter 'list artists'."
-    puts "To list all of the genres in your library, enter 'list genres'."
-    puts "To list all of the songs by a particular artist, enter 'list artist'."
-    puts "To list all of the songs of a particular genre, enter 'list genre'."
-    puts "To play a song, enter 'play song'."
-    puts "To quit, type 'exit'."
-    puts "What would you like to do?"
-    input = gets
+      puts "Welcome to your music library!"
+      puts "To list all of your songs, enter 'list songs'."
+      puts "To list all of your songs, enter 'list songs'."
+      puts "To list all of the artists in your library, enter 'list artists'."
+      puts "To list all of the genres in your library, enter 'list genres'."
+      puts "To list all of the songs by a particular artist, enter 'list artist'."
+      puts "To list all of the songs of a particular genre, enter 'list genre'."
+      puts "To play a song, enter 'play song'."
+      puts "To quit, type 'exit'."
+      puts "What would you like to do?"
+      input = gets
 
-    case input
-    when "list songs"
-      Song.all
-    when "list artists"
-      Artist.all
-    when "list genres"
-      Genre.all
-    when "list artist"
-      puts "Which artist?"
-      artist = gets
-      Artist.find_by_name(artist.songs {|song| puts song.name}
-    when "list genre"
-      puts "Which genre?"
-      genre = gets
-      Genre.find_by_name(genre).songs {|song| puts song.name}
-    when "play song"
-      puts "song playing"
-    else
-      puts "Please choose from the list above"
+      case input
+      when "list songs"
+        Song.all
+      when "list artists"
+        Artist.all
+      when "list genres"
+        Genre.all
+      when "list artist"
+        puts "Which artist?"
+        artist = gets
+        Artist.find_by_name(artist.songs {|song| puts song.name}
+      when "list genre"
+        puts "Which genre?"
+        genre = gets
+        Genre.find_by_name(genre).songs {|song| puts song.name}
+      when "play song"
+        puts "song playing"
+      else
+        puts "Please choose from the list above"
+      end
     end
-
   end
 
 end
